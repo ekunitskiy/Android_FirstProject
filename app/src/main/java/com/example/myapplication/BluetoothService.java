bceca9a3ff9cc9ebc7b6b07262b468f528470301
Evgen <studentkpimag@gmail.com>
2023-09-25 20:01:43 +0300

    get start logic for Bluetooth

--- app/src/main/java/com/example/myapplication/BluetoothService.java
+++ app/src/main/java/com/example/myapplication/BluetoothService.java
@@ -13,5 +13,37 @@ import java.io.OutputStream;
 import java.util.UUID;
 
 public class BluetoothService {
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothDevice connectedDevice;
+    private BluetoothSocket bluetoothSocket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    public BluetoothService() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
 
+    // Методи для підключення та передачі файлів через Bluetooth
+
+    public boolean connectToDevice(BluetoothDevice device) {
+        // Логіка для підключення до пристрою Bluetooth
+        // Повертає true, якщо підключення вдалось
+        return false;
+    }
+
+    public boolean sendFile(File file) {
+        // Логіка для передачі файлу через Bluetooth
+        // Повертає true, якщо передача вдалась
+        return false;
+    }
+
+    public byte[] receiveFile() {
+        // Логіка для отримання файлу через Bluetooth
+        // Повертає отриманий файл у вигляді байтового масиву
+        return new byte[]{};
+    }
+
+    public void closeConnection() {
+        // Логіка для закриття Bluetooth-з'єднання та потоків
+    }
 }
+
